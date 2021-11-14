721eb319ad10eb40aa96c0215073fc988094ba9f
Christian Fei <crifei93@gmail.com>
2021-11-14 13:12:29 +0100

    redirect / to /products

--- lib/amazon_tracker_web/controllers/page_controller.ex
+++ lib/amazon_tracker_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule AmazonTrackerWeb.PageController do
   use AmazonTrackerWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    # render(conn, "index.html")
+    redirect(conn, to: "/products")
   end
 end
